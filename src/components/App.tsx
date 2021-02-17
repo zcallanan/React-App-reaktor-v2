fdec5f892174b2442e78b1d6916647d78914537f
Zach Callanan <caveofmonsters@gmail.com>
2021-02-17 15:29:33 +0100

    Testing packages, simplified Router component, selectedProduct() now checks if an element is not null

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -60,10 +60,17 @@ class App extends React.Component<Props, State> {
   };
 
   protected getProductList(product: string): void {
-    const webToken: string = process.env.REACT_APP_WEB_TOKEN!
-    const productStatus: productStatusType = { ...this.state.productStatus };
     const controller = this.state.controller;
     const signal = controller.signal;
+    let url: string;
+    const webToken: string = process.env.REACT_APP_WEB_TOKEN!
+
+    if (process.env.NODE_ENV === 'test') {
+      // Testing requires a string
+      url = 'http://localhost:3010/';
+    } else {
+      url = process.env.REACT_APP_PROXY_URL!
+    }
 
     const headers: HeadersInit = {
       'X-WEB-TOKEN': webToken,
@@ -75,10 +82,6 @@ class App extends React.Component<Props, State> {
       headers,
       signal
     }
-    const url: string = process.env.REACT_APP_PROXY_URL!
-
-    productStatus.pendingProduct = true;
-    this.setState({ productStatus });
 
     const fetchProducts = async (url: string, opts: RequestInit): Promise<productsType | undefined> => {
       let data: productsAPIType;
@@ -105,6 +108,8 @@ class App extends React.Component<Props, State> {
           // TODO: Data is empty, handle it
           const productStatus: productStatusType = { ...this.state.productStatus };
           if (!productStatus.successProduct) {
+            productStatus.pendingProduct = true;
+            this.setState({ productStatus });
             fetchProducts(url, opts);
           }
 
@@ -153,13 +158,19 @@ class App extends React.Component<Props, State> {
     // Select the nav product in view at load
     if (this.props.slug === 'beanies') {
       const beaniesLink: Element | null = document.querySelector('.beanies-link');
-      beaniesLink!.classList.add('active');
+      if (beaniesLink !== null) {
+        beaniesLink!.classList.add('active');
+      }
     } else if (this.props.slug === 'facemasks') {
       const facemasksLink: Element | null = document.querySelector('.facemasks-link');
-      facemasksLink!.classList.add('active');
+      if (facemasksLink !== null) {
+        facemasksLink!.classList.add('active');
+      }
     } else if (this.props.slug === 'gloves') {
       const glovesLink: Element | null = document.querySelector('.gloves-link');
-      glovesLink!.classList.add('active');
+      if (glovesLink !== null) {
+        glovesLink!.classList.add('active');
+      }
     }
   }
 
@@ -193,14 +204,16 @@ class App extends React.Component<Props, State> {
           />
         </div>
       )
+    } else {
+      return (
+        <div className="spinner-div">
+          <Spinner animation="border" role="status" variant="primary" className="product-spinner">
+            <span className="sr-only">Loading...</span>
+          </Spinner>
+        </div>
+      )
     }
-    return (
-      <div className="spinner-div">
-        <Spinner animation="border" role="status" variant="primary" className="product-spinner">
-          <span className="sr-only">Loading...</span>
-        </Spinner>
-      </div>
-    )
+
   }
 
 }
---
+++ src/tests/Product.test.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Router from '../components/Router';
+import Beanies from '../components/Beanies';
+import Facemasks from '../components/Facemasks';
+import Gloves from '../components/Gloves';
+import { MemoryRouter } from "react-router-dom";
+import { act } from 'react-dom/test-utils';
+
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { mount } from 'enzyme';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('<Glove> Component Tests', () => {
+  fetch.mockResponse(JSON.stringify({"rows":
+    [
+      {"id":"7c3149c69aeb51e167d6","type":"gloves","name":"AKFOLHEM EARTH","color":"red","price":11,"manufacturer":"juuran","availability":"In Stock"},
+      {"id":"da35e621ad262615e2fa9e5","type":"gloves","name":"DALLEAKOL NORMAL","color":"red","price":64,"manufacturer":"juuran","availability":"Less Than 10"},
+      {"id":"1b1610b94d3d1d701b0","type":"gloves","name":"HEMFOLNY SLIP METROPOLIS","color":"black","price":54,"manufacturer":"juuran","availability":"Out of Stock"}
+    ]
+  }))
+  test('Glove Product List component displayed', async () => {
+    let component;
+    await act( async () => {
+      component = mount(<MemoryRouter initialEntries={["/v2/gloves"]} ><Gloves slug="gloves" /></MemoryRouter>)
+    });
+    component.update()
+    expect(component.find('ProductList')).toHaveLength(1);
+    component.unmount()
+  })
+
+  test('Product Card components are displayed', async () => {
+    let component;
+    await act( async () => {
+      component = mount(<MemoryRouter initialEntries={["/v2/gloves"]} ><Gloves slug="gloves" /></MemoryRouter>)
+    });
+    component.update()
+    expect(component.find('ProductCard')).toHaveLength(3);
+    component.unmount()
+  })
+
+  test('Glove product data is displayed', async () => {
+    let component;
+    await act( async () => {
+      component = mount(<MemoryRouter initialEntries={["/v2/gloves"]} ><Gloves slug="gloves" /></MemoryRouter>)
+    });
+    component.update()
+    // console.log(component.debug())
+    expect(component.text().includes('Akfolhem Earth')).toBe(true);
+    component.unmount()
+  })
+})
