735b3a74de7e9c55253986e8bb048eeab7def026
Zach Callanan <caveofmonsters@gmail.com>
2021-02-18 12:39:12 +0100

    Combined/added product card tests

--- src/tests/Beanies.test.tsx
+++ src/tests/Beanies.test.tsx
@@ -15,8 +15,8 @@ describe('<Beanies> Component Tests', () => {
   fetch.mockResponse(JSON.stringify({"rows":
     [
       {"id":"03f808a0c79173c1c934aad3","type":"beanies","name":"JEREVUP TYRANNUS","color":"purple","price":82,"manufacturer":"ippal","availability":"In Stock"},
-      {"id":"34a029d74de58141a5c5e9","type":"beanies","name":"OOTUPTAI RAIN METROPOLIS","color":"red","price":96,"manufacturer":"abiplos","availability":"Less Than 10"},
-      {"id":"668c7c0c10f689f164b","type":"beanies","name":"HEMOOT BOON","color":"grey","price":76,"manufacturer":"abiplos","availability":"Out of Stock"}
+      {"id":"34a029d74de58141a5c5e9","type":"beanies","name":"OOTUPTAI RAIN METROPOLIS","color":"red","price":96,"manufacturer":"hennex","availability":"Less Than 10"},
+      {"id":"668c7c0c10f689f164b","type":"beanies","name":"HEMOOT BOON","color":"grey, green","price":76,"manufacturer":"abiplos","availability":"Out of Stock"}
     ]
   }))
 
@@ -30,23 +30,28 @@ describe('<Beanies> Component Tests', () => {
     component.unmount()
   })
 
-  test('Beanies Product Card components are displayed', async () => {
+  test('Beanies Product Card components and data are displayed', async () => {
     let component;
     await act( async () => {
       component = mount(<MemoryRouter initialEntries={["/v2/beanies"]} ><Beanies slug="beanies" /></MemoryRouter>)
     });
     component.update()
     expect(component.find('ProductCard')).toHaveLength(3);
-    component.unmount()
-  })
-
-  test('Beanies product data is displayed', async () => {
-    let component;
-    await act( async () => {
-      component = mount(<MemoryRouter initialEntries={["/v2/beanies"]} ><Beanies slug="beanies" /></MemoryRouter>)
-    });
-    component.update()
-    expect(component.text().includes('Hemoot Boon')).toBe(true);
+    expect(component.find('.product-manufacturer').first().text().includes('Manufacturer: Ippal')).toBe(true);
+    expect(component.find('.product-manufacturer').at(1).text().includes('Manufacturer: Hennex')).toBe(true);
+    expect(component.find('.product-manufacturer').last().text().includes('Manufacturer: Abiplos')).toBe(true);
+    expect(component.find('.product-name').first().text().includes('Name: Jerevup Tyrannus')).toBe(true);
+    expect(component.find('.product-name').at(1).text().includes('Name: Ootuptai Rain Metropolis')).toBe(true);
+    expect(component.find('.product-name').last().text().includes('Name: Hemoot Boon')).toBe(true);
+    expect(component.find('.product-colors').first().text().includes('Colors: Purple')).toBe(true);
+    expect(component.find('.product-colors').at(1).text().includes('Colors: Red')).toBe(true);
+    expect(component.find('.product-colors').last().text().includes('Colors: Grey, Green')).toBe(true);
+    expect(component.find('.product-price').first().text().includes('Price: 82 €')).toBe(true);
+    expect(component.find('.product-price').at(1).text().includes('Price: 96 €')).toBe(true);
+    expect(component.find('.product-price').last().text().includes('Price: 76 €')).toBe(true);
+    expect(component.find('.product-availability').first().text().includes('In Stock')).toBe(true);
+    expect(component.find('.product-availability').at(1).text().includes('Less Than 10')).toBe(true);
+    expect(component.find('.product-availability').last().text().includes('Out of Stock')).toBe(true);
     component.unmount()
   })
 })
--- src/tests/Facemasks.test.tsx
+++ src/tests/Facemasks.test.tsx
@@ -14,8 +14,8 @@ describe('<Facemasks> Component Tests', () => {
   fetch.mockResponse(JSON.stringify({"rows":
     [
       {"id":"ccd1da35142c4d7ece61b7","type":"facemasks","name":"AKTAIFOL FANTASY","color":"white","price":11,"manufacturer":"niksleh","availability":"In Stock"},
-      {"id":"1fc9a15517b1d22953c","type":"facemasks","name":"EMAK FLOWER","color":"purple","price":45,"manufacturer":"okkau","availability":"Less Than 10"},
-      {"id":"6825e2648c4af44cf971a4","type":"facemasks","name":"MOHEMLEA ROOM","color":"blue","price":94,"manufacturer":"laion","availability":"Out of Stock"}
+      {"id":"1fc9a15517b1d22953c","type":"facemasks","name":"EMAK FLOWER","color":"purple, black","price":45,"manufacturer":"okkau","availability":"Less Than 10"},
+      {"id":"6825e2648c4af44cf971a4","type":"facemasks","name":"MOHEMLEA ROOM","color":"blue, green","price":94,"manufacturer":"laion","availability":"Out of Stock"}
     ]
   }))
 
@@ -29,23 +29,28 @@ describe('<Facemasks> Component Tests', () => {
     component.unmount()
   })
 
-  test('Facemasks Product Card components are displayed', async () => {
+  test('Facemasks Product Card components and data are displayed', async () => {
     let component;
     await act( async () => {
       component = mount(<MemoryRouter initialEntries={["/v2/facemasks"]} ><Facemasks slug="facemasks" /></MemoryRouter>)
     });
     component.update()
     expect(component.find('ProductCard')).toHaveLength(3);
-    component.unmount()
-  })
-
-  test('Facemasks product data is displayed', async () => {
-    let component;
-    await act( async () => {
-      component = mount(<MemoryRouter initialEntries={["/v2/facemasks"]} ><Facemasks slug="facemasks" /></MemoryRouter>)
-    });
-    component.update()
-    expect(component.text().includes('Emak Flower')).toBe(true);
+    expect(component.find('.product-manufacturer').first().text().includes('Manufacturer: Niksleh')).toBe(true);
+    expect(component.find('.product-manufacturer').at(1).text().includes('Manufacturer: Okkau')).toBe(true);
+    expect(component.find('.product-manufacturer').last().text().includes('Manufacturer: Laion')).toBe(true);
+    expect(component.find('.product-name').first().text().includes('Name: Aktaifol Fantasy')).toBe(true);
+    expect(component.find('.product-name').at(1).text().includes('Name: Emak Flower')).toBe(true);
+    expect(component.find('.product-name').last().text().includes('Name: Mohemlea Room')).toBe(true);
+    expect(component.find('.product-colors').first().text().includes('Colors: White')).toBe(true);
+    expect(component.find('.product-colors').at(1).text().includes('Colors: Purple, Black')).toBe(true);
+    expect(component.find('.product-colors').last().text().includes('Colors: Blue, Green')).toBe(true);
+    expect(component.find('.product-price').first().text().includes('Price: 11 €')).toBe(true);
+    expect(component.find('.product-price').at(1).text().includes('Price: 45 €')).toBe(true);
+    expect(component.find('.product-price').last().text().includes('Price: 94 €')).toBe(true);
+    expect(component.find('.product-availability').first().text().includes('In Stock')).toBe(true);
+    expect(component.find('.product-availability').at(1).text().includes('Less Than 10')).toBe(true);
+    expect(component.find('.product-availability').last().text().includes('Out of Stock')).toBe(true);
     component.unmount()
   })
 })
